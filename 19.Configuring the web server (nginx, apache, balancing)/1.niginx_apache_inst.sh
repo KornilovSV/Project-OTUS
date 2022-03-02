9cb7d75b1bc1faceff6ec784558c85b4f7ca61ad
KSV <kornilovsv@yandex.ru>
2022-03-02 16:10:59 +0500

    Edit

--- 19.Configuring the web server (nginx, apache, balancing)/1.niginx_apache_inst.sh
+++ 19.Configuring the web server (nginx, apache, balancing)/1.niginx_apache_inst.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # Install niginx_apache:
 
-yum install epel-release && \
-yum install -y nginx httpd && \
+yum install -y epel-release nginx httpd && \
 
 systemctl start nginx && systemctl status nginx
-ps -fax | grep -A 3 nginx
-ss -tlnp | grep -A 2 "*:80"
-# curl localhost 80
+ss -tlnp | grep "*:80"
 sleep 5
-# systemctl enable nginx && systemctl is-enabled nginx
+curl localhost | head
+sleep 5
+systemctl enable nginx && systemctl is-enabled nginx
 sleep 3
 systemctl stop nginx
 
 systemctl start httpd && systemctl status httpd
-ps -fax | grep -A 3 httpd
-ss -tlnp | grep -A 2 "*:80"
-# curl localhost 80
+ss -tlnp | grep "*80"
+sleep 5
+curl localhost | head
+sleep 5
 systemctl enable httpd && systemctl is-enabled httpd
 sleep 5
-ps fax | grep httpd
+systemctl stop httpd
 
 # END
