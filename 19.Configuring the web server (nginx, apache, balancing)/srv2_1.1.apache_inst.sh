2a82d71ac9aefb487c091babb15c808c19c11b2f
KSV <kornilovsv@yandex.ru>
2022-03-03 15:46:37 +0500

    del file

---
+++ 19.Configuring the web server (nginx, apache, balancing)/srv2_1.1.apache_inst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install apache:
+
+yum install -y epel-release httpd && \
+
+systemctl start httpd && \
+clear && \
+ss -tlnp | grep "*80"
+sleep 7
+clear && \
+curl localhost | head
+sleep 7
+clear && \
+systemctl enable httpd && \
+
+echo "2-nd Autostart Apache:"
+systemctl is-enabled httpd
+
+# END
