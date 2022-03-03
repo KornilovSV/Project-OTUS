18eb16a2eb63cb1cbc590cd3cf98c0f30c9c2b87
KSV <kornilovsv@yandex.ru>
2022-03-03 15:07:08 +0500

    srv1_1.niginx_apache_inst.sh v.1.1

--- 19.Configuring the web server (nginx, apache, balancing)/srv1_1.niginx_apache_inst.sh
+++ 19.Configuring the web server (nginx, apache, balancing)/srv1_1.niginx_apache_inst.sh
@@ -8,21 +8,22 @@ yum install -y httpd && systemctl stop httpd && \
 systemctl start nginx && \
 clear && \
 ss -tlnp | grep "*:80"
-sleep 5
+sleep 7
 clear && \
 curl localhost | head
-sleep 5
+sleep 7
 systemctl enable nginx && \
 systemctl stop nginx && \
 
 systemctl start httpd && \
 clear && \
 ss -tlnp | grep "*80"
-sleep 5
+sleep 7
 clear && \
 curl localhost | head
-sleep 5
+sleep 7
 systemctl disable --now httpd && \
+clear && \
 
 echo "Autostart Nginx, Apache:"
 systemctl is-enabled nginx && \
