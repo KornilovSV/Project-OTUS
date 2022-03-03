92b562ac77de30e3f9fafc39d8100afa6425397d
KSV <kornilovsv@yandex.ru>
2022-03-03 16:17:27 +0500

    edit conf.sh v.2

--- 19.Configuring the web server (nginx, apache, balancing)/srv1_1.2.niginx_apache_conf.sh
+++ 19.Configuring the web server (nginx, apache, balancing)/srv1_1.2.niginx_apache_conf.sh
@@ -4,12 +4,13 @@
 # Backup orig conf file:
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf_orig && \
 cp srv1_1.2_nginx_main.conf /etc/nginx/nginx.conf && \
-cp srv1_1.3_nginx_backend.conf /etc/nginx/
+cp srv1_1.3_nginx_backend.conf /etc/nginx/conf.d/backend.conf
 
+systemctl start nginx && \
 clear  && \
 nginx -t && \
 sleep 7
-systemctl reload nginx && \
+# systemctl reload nginx && \
 
 # Copy httpd
 # clear  && \
