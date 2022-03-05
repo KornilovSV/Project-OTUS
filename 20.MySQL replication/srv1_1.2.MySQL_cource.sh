2601a85057195489c7d07c17a7b1956080b56bbb
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-05 21:27:58 +0500

    srv1_1.2.MySQL_cource.sh

--- 20.MySQL replication/srv1_1.2.MySQL_cource.sh
+++ 20.MySQL replication/srv1_1.2.MySQL_cource.sh
@@ -1,19 +1,16 @@
 #####################################################
-# На Мастере
+# On MySQL Source
 #####################################################
 
-# Создаём пользователя для реплики в MySQL
-mysql -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';"
+# Creating a user for a replica in MySQL
+mysql -e "CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';" && \
 
-# Даём ему права на репликацию
-GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+# Replication rights for the "repl" user
+mysql -e "GRANT REPLICATION SLAVE ON *.* TO repl@'%';" && clear && \
 
-# Сбрасываем кэш привилегий (не обязательно)
-# FLUSH PRIVILEGES;
+# Checking the user "repl"
+mysql -e "SELECT User, Host FROM mysql.user;" && sleep 5 && \
 
-# Смотрим пользователей
-SELECT User, Host FROM mysql.user;
-
-# Смотрим статус Мастера
-SHOW MASTER STATUS;
+# Master status
+mysql -e "SHOW MASTER STATUS;"
 
