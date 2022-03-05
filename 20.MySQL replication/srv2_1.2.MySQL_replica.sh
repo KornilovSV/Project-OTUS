af24bc152ed82838b4895c04a53e2fac2ac8def7
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-05 23:31:56 +0500

    MySQL_change_src

--- 20.MySQL replication/srv2_1.2.MySQL_replica.sh
+++ 20.MySQL replication/srv2_1.2.MySQL_replica.sh
@@ -17,26 +17,28 @@ mysql -e "select @@server_id;" && sleep 3 && \
 # необходимо получить публичный ключ
 mysql -e "STOP SLAVE;" && \
 
-# CHANGE MASTER TO MASTER_HOST='192.168.4.50', MASTER_USER='repl', MASTER_PASSWORD='oTUSlave#2020', MASTER_LOG_FILE='binlog.000XXX', MASTER_LOG_POS=XXX, GET_MASTER_PUBLIC_KEY = 1;
-nano ./srv2_1.3.MySQL_change_src && \
-mysql -e "START SLAVE;" && mysql -e "show slave status\G"
+echo "Change: Master_IP, Binlog_number, POS_number: "
+echo "CHANGE MASTER TO MASTER_HOST='192.168.4.50', MASTER_USER='repl', MASTER_PASSWORD='oTUSlave#2020', MASTER_LOG_FILE='binlog.000XXX', MASTER_LOG_POS=XXX, GET_MASTER_PUBLIC_KEY = 1;"
+echo "And paste this line into: mysql > " && sleep 5 && \
+mysql -uroot && mysql -e "START SLAVE;" && mysql -e "show slave status\G"
 
 # https://dev.mysql.com/doc/refman/8.0/en/replica-logs-relaylog.html
 # можем настроить наш relay.log
-show variables like '%relay%';
+# show variables like '%relay%';
 
 # посмотрим статусы репликации
-use performance_schema;
-show tables like '%replic%';
-show variables like '%log_bin%';
-show variables like '%binlog%';
-show variables like '%read%';
+# use performance_schema;
+# show tables like '%replic%';
+# show variables like '%log_bin%';
+# show variables like '%binlog%';
+# show variables like '%read%';
 
 # рекомендуется включать для слейва, иначе на слейве тоже можем вносить изменения
 # /etc/my.cnf
-innodb_read_only = 1
+# innodb_read_only = 1
 
 
 # КОМАНДЫ ДЛЯ УПРАВЛЕНИЯ СОСТОЯНИЕМ
-STOP SLAVE;
-START SLAVE;
+# STOP SLAVE;
+# START SLAVE;
+
