82675e466a07e08bc798dad1bd82dcc50ac27e94
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-06 20:29:29 +0500

    cron file

--- Cron/cron.sh
+++ Cron/cron.sh
@@ -3,6 +3,7 @@
 
 mkdir /home/BackUp/ && \
 cp mysql-backup.sh /home/BackUp/ && \
+cp git-pull.sh ~/Git/git-pull.sh && \
 cp cron /var/spool/cron/root && \
 
 systemctl status crond.service -l
---
+++ Cron/git-pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Git pull
+cd ~/Git/Project-OTUS/ && git pull
+
+#
