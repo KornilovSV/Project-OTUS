a4d57b5d33d7b0dd73fc1fbfc03744dae5562012
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-06 17:04:12 +0500

    mysql-backup.sh work ver.

--- 20.MySQL replication/mysql-backup.sh
+++ 20.MySQL replication/mysql-backup.sh
@@ -2,27 +2,31 @@
 # MySQL 8.0 Backup script
 
 dtime=$(date "+%F_%R")
-
-# Dump database:
-MySQL='mysql --user=root --skip-column-names'
+mkdir $dtime && cd $dtime
 
 # Stop Slave:
 mysql -e "stop slave;" && \
 echo "Slave status: "
 mysql -e "show slave status\G" | grep Running
 
-for d in mysql `$MySQL -e "SHOW DATABASES"`;
-do
-   mkdir '$d_$dtime'
-   /usr/bin/mysqldump -uroot --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers $d | gzip -1 > ./$d/$d.gz;
+MySQL='mysql --skip-column-names'
 
-#   for t in $(mysql -uroot $d -e "show tables;"|grep -v Tables_in_);
-#     do
-#     /usr/bin/mysqldump -uroot --add-locks --quick --create-options --extended-insert --single-transaction --set-charset --master-data=2 $d $t 2> /dev/null | gzip > ./$d/$t".gz";
-#     done
+for db in mysql `$MySQL -e "SHOW DATABASES"`;
+do
+   mkdir $db;
+   mkdir full_$db;
+   /usr/bin/mysqldump -uroot --opt --single-transaction --events --routines --triggers --master-data=2 $db | gzip -1 > ./full_$db/$db.gz;
+
+   for tbl in $(mysql -uroot $db -e "show tables;"|grep -v Tables_in_);
+     do
+     /usr/bin/mysqldump -uroot --opt --single-transaction --master-data=2 $db $tbl 2> /dev/null | gzip -1 > ./$db/$tbl".gz";
+     done
 done
 
 # Start Slave:
 mysql -e "start slave;" && \
 echo "Slave status: "
 mysql -e "show slave status\G" | grep Running
+cd ..
+
+# END
