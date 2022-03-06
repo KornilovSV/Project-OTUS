341f4e5cd44e24c512c62536d7aac00526d7a4e1
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-06 19:46:11 +0500

    2.nginx, apache

---
+++ 2.nginx_apache/srv1_1.1.niginx_apache_inst.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Install niginx_apache:
+
+yum install -y epel-release nginx && \
+systemctl stop nginx && \
+yum install -y httpd && systemctl stop httpd && \
+
+systemctl start nginx && \
+clear && \
+ss -tlnp | grep "nginx"
+sleep 7
+clear && \
+curl localhost | head
+sleep 7
+systemctl enable nginx && \
+systemctl stop nginx && \
+
+systemctl start httpd && \
+clear && \
+ss -tlnp | grep "httpd"
+sleep 7
+clear && \
+curl localhost:8081 | head
+sleep 7
+systemctl disable --now httpd && \
+clear && \
+
+echo "Autostart Nginx, Apache:"
+systemctl is-enabled nginx && \
+systemctl is-enabled httpd
+
+# END
---
+++ 2.nginx_apache/srv2_1.1.apache_inst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install apache:
+
+yum install -y epel-release httpd && \
+systemctl start httpd && \
+clear && \
+ss -tlnp | grep "httpd"
+sleep 7
+clear && \
+curl localhost | head
+sleep 7
+clear && \
+systemctl enable httpd && \
+clear && \
+
+echo "Autostart Apache:"
+systemctl is-enabled httpd
+
+# END
