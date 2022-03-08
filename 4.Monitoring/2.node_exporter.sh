18594404867b9252b4b050c10f2e0ad98e3c14df
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-08 17:11:16 +0500

    node_exporter.service

--- 4.Monitoring/2.node_exporter.sh
+++ 4.Monitoring/2.node_exporter.sh
@@ -9,56 +9,34 @@ curl -LO https://github.com/prometheus/node_exporter/releases/download/v$ver/nod
 pkg=$(ls | grep tar.gz) && tar -xvf $pkg && \
 useradd --no-create-home --shell /usr/sbin/nologin node_exporter && \
 
----
 # Редактируем файл кофигурации:
 nano ~/Project-OTUS/4.Monitoring/prometheus.yml
 nano /etc/prometheus/prometheus.yml && \
 
----
+#---
 systemctl restart prometheus.service && systemctl status prometheus.service & \
 
----
 # Копируем сам node_exporter:
 cp -vi ./node_exporter-$ver.linux-amd64/node_exporter /usr/local/bin/ && \
 chown node_exporter: /usr/local/bin/node_exporter && \
 ls -l /usr/local/bin/
+sleep 5
 
----
 # Создаем юнит:
-cat > /etc/systemd/system/node_exporter.service
+cp -iv ~/Git/Project-OTUS/4.Monitoring/node_exporter.service \
+/etc/systemd/system/node_exporter.service
 
-[Unit]
-Description=Node Exporter
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-User=node_exporter
-Group=node_exporter
-Type=simple
-ExecStart=/usr/local/bin/node_exporter
-
-[Install]
-WantedBy=multi-user.target
-
-#; Ctrl+C
-
----
 # Стартуем node_exporter:
 systemctl daemon-reload && \
 systemctl start node_exporter.service && \
-systemctl status node_exporter.service
+systemctl status node_exporter.service && \
+clear && sleep 5
 
-# ● node_exporter.service - Node Exporter
-#   Loaded: loaded (/etc/systemd/system/node_exporter.service; disabled; vendor preset: disabled)
-#   Active: active (running) since Sat 2022-02-26 01:54:52 +05; 7s ago
-# Main PID: 7920 (node_exporter)
-
----
 # Проверяем:
 curl localhost:9100
 
 # Добавляем в автозагрузку
 systemctl enable node_exporter.service
 
----------------------------------------
+#END
+
