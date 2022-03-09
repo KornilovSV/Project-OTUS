2eef064e2b443357539124e31b8fd6fb3106ea72
KSV <kornilovsv@yandex.ru>
2022-03-09 15:07:19 +0500

    cos7-preinstall

--- 1.Git/cos7-preinstall.sh
+++ 1.Git/cos7-preinstall.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+# Preinstall CentOS 7
+
+yum check-update && yum update && \
+yum -y install nano wget tmux lynx mc && \
+cp -iv tmux.conf /etc/tmux.conf && clear && \
+tmux -V && sleep 5 && vi /etc/tmux.conf
+
+setenforce 0 && clear && getenforce && \
+systemctl disable --now firewalld && iptables -L
+
