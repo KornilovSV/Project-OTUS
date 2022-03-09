d8303b8f6c3fc787ded74130b72aed06de41702a
KSV <kornilovsv@yandex.ru>
2022-03-09 15:19:41 +0500

    cos7-preinstall

--- 1.Git/cos7-preinstall.sh
+++ 1.Git/cos7-preinstall.sh
@@ -6,7 +6,7 @@ yum -y install nano wget tmux lynx mc && \
 cp -iv tmux.conf /etc/tmux.conf && clear && \
 tmux -V && sleep 5 && vi /etc/tmux.conf
 
-setenforce 0 && clear && getenforce && \
-systemctl disable --now firewalld && cleep 5 && \
-iptables -L && sleep 5 && mc
+systemctl disable --now firewalld && \
+setenforce 0 && getenforce && iptables -L
+sleep 5 && mc
 
