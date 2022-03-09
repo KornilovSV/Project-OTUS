992032445a2d2533ec5496f939f601397a61b8e2
KSV <kornilovsv@yandex.ru>
2022-03-09 15:12:13 +0500

    cos7-preinstall

--- 1.Git/cos7-preinstall.sh
+++ 1.Git/cos7-preinstall.sh
@@ -7,5 +7,6 @@ cp -iv tmux.conf /etc/tmux.conf && clear && \
 tmux -V && sleep 5 && vi /etc/tmux.conf
 
 setenforce 0 && clear && getenforce && \
-systemctl disable --now firewalld && iptables -L
+systemctl disable --now firewalld && cleep 5 && \
+iptables -L && sleep 5 && mc
 
