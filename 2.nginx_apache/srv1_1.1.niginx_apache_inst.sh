4418a0e8fba7c8977fe33f83ad67cede225c35ba
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-12 12:26:50 +0500

    Update srv1.sh

--- 2.nginx_apache/srv1_1.1.niginx_apache_inst.sh
+++ 2.nginx_apache/srv1_1.1.niginx_apache_inst.sh
@@ -2,16 +2,19 @@
 # Install niginx_apache:
 # Developed by KornilovSV
 
+# Install packages:
 yum install -y epel-release nginx && \
 systemctl stop nginx && \
 yum install -y httpd && systemctl stop httpd && \
 
+# Status nginx:
 systemctl start nginx && \
 clear && ss -tlnp | grep "nginx" && sleep 7 &&\
 clear && curl localhost | head && sleep 7 && \
 systemctl enable nginx && \
 systemctl stop nginx && \
 
+# Status httpd:
 systemctl start httpd && \
 clear && ss -tlnp | grep "httpd" && sleep 7 && \
 clear && curl localhost | head && sleep 5 && \
