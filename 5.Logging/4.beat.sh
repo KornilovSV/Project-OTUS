623a538bc7822543181e5e99783b5b49d2fb9431
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-14 00:36:37 +0500

    Update 4.beat..sh

--- 5.Logging/4.beat.sh
+++ 5.Logging/4.beat.sh
@@ -11,7 +11,7 @@ chmod 600 /etc/filebeat/filebeat.yml
 
 # Start Filebeat
 systemctl enable --now filebeat && sleep 5 && \
-systemctl restart filebeat && \
+# systemctl restart filebeat && \
 clear && systemctl status filebeat && sleep 5
 
 #END
