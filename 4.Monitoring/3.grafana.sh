d86fbf194d4e6ef16fa20bf970f994bace87c81e
Sergey V. Kornilov <kornilovsv@yandex.ru>
2022-03-15 01:49:57 +0500

    Update 3.grafana.sh

--- 4.Monitoring/3.grafana.sh
+++ 4.Monitoring/3.grafana.sh
@@ -7,10 +7,13 @@ echo -n "Enter current version of Grafana: "
 read ver
 
 curl -LO https://dl.grafana.com/oss/release/grafana-$ver.x86_64.rpm && \
-pkg=$(ls grafana*) && yum install -y $pkg && sleep 5 && \
+pkg=$(ls grafana*) && yum install -y $pkg && \
+
+echo "Start Grafana"
 systemctl daemon-reload && sleep 10 && \
-systemctl start grafana-server && sleep 10 && clear && \
-systemctl status grafana-server && systemctl enable grafana-server && \
+systemctl enable grafana-server && \
+systemctl start grafana-server && \
+clear && systemctl status grafana-server && \
 
 sleep 5 && clear
 curl http://localhost:3000 | head
